1a9f931dd3e5420f497ddb7cb627083978f3ca29
Никита Брагин <branikita@gmail.com>
2013-04-09 04:13:49 +0400

    Add router

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('./src/soshace.js');
 require('./src/soshace/db.js');
 require('./src/soshace/server.js');
+require('./src/soshace/router.js');
 
 var APP = {
     init: function(){
@@ -12,33 +13,6 @@ var APP = {
 
 Soshace.Db.instance('soshace', function(){
     Soshace.Server.instance(80, function(response, request){
-
+          Soshace.Router.instance(response, request);
     });
 });
-
-
-
-
-
-
-//var Routs = {};
-//Routs['/'] = function(request, callback){
-//    callback({statusCode: 200, contentType: 'text/html', body: 'Hello world!!!'})
-//}
-//var server = Soshace.Server.instance();
-//server.addRouts(Routs).start()
-
-
-//Soshace.Db.instance().getCollection('ru_countries', function(data){
-//    if(data.error){
-//        return;
-//    }
-//    if(data.collection){
-//        data.collection.find().toArray(function(error, docs) {
-//            if(error){
-//                return;
-//            }
-//            console.log(docs);
-//        });
-//    }
-//});
--- src/soshace/router.js
+++ src/soshace/router.js
@@ -1,9 +1,92 @@
-//    on: function(request, response){
-//        var pathName = url.parse(request.url).pathname;
-//
-//        this.routs[pathName](request, function(data){
-//            response.writeHead(data.statusCode, {"Content-Type": data.contentType});
-//            response.write(data.body);
-//            response.end();
-//        });
-//    }
+var url = require('url');
+
+Soshace.Router = Soshace.extend({
+    /**
+     * Относительный URL
+     */
+    rout: '',
+    /**
+     * Распарсенный URL
+     */
+    routParams: null,
+    /**
+     * @constructor
+     * @param request
+     * @param response
+     */
+    init:function(request, response){
+        this.rout = url.parse(request.url).pathname;
+        this.routParams = {
+            language: '',
+            country: '',
+            city: '',
+            category:'',
+            subcategory:'',
+            postId:''
+        };
+        this.routGetParams();
+        console.log(this.routParams);
+    },
+    /**
+     * Метод, который парсит URL
+     * @function
+     */
+    routGetParams: function(){
+        var params = [];
+        if(this.rout === '') {
+            return;
+        }
+
+        // Пример: /ru
+        if(/^\/\w+\/?$/.test(this.rout)){
+            params = this.rout.match(/^\/(\w+)\/?$/);
+            this.routParams.language = params[1];
+            return;
+        }
+
+        // Пример: /ru/events
+        if(/^\/\w+\/(?:events|places|persons)\/?$/.test(this.rout)){
+            params = this.rout.match(/^\/(\w+)\/(events|places|persons)\/?$/);
+            this.routParams.language = params[1];
+            this.routParams.category = params[2];
+            return;
+        }
+
+        // Пример: /ru/russia
+        if(/^\/\w+\/\w+\/?$/.test(this.rout)){
+            params = this.rout.match(/^\/(\w+)\/(\w+)\/?$/);
+            this.routParams.language = params[1];
+            this.routParams.country = params[2];
+            return;
+        }
+
+        // Пример: /ru/russia/events
+        if(/^\/\w+\/\w+\/(?:events|places|persons)\/?$/.test(this.rout)){
+            params = this.rout.match(/^\/(\w+)\/(\w+)\/(events|places|persons)\/?$/);
+            this.routParams.language = params[1];
+            this.routParams.country = params[2];
+            this.routParams.category = params[3];
+            return;
+        }
+
+        // Пример: /ru/russia/msk
+        if(/^\/\w+\/\w+\/\w+\/?$/.test(this.rout)){
+            params = this.rout.match(/^\/(\w+)\/(\w+)\/(\w+)\/?$/);
+            this.routParams.language = params[1];
+            this.routParams.country = params[2];
+            this.routParams.city = params[3];
+            return;
+        }
+
+        // Пример: /ru/russia/msk/events
+        if(/^\/\w+\/\w+\/\w+\/(?:events|places|persons)\/?$/.test(this.rout)){
+            params = this.rout.match(/^\/(\w+)\/(\w+)\/(\w+)\/(events|places|persons)\/?$/);
+            this.routParams.language = params[1];
+            this.routParams.country = params[2];
+            this.routParams.city = params[3];
+            this.routParams.category = params[4];
+            return;
+        }
+    }
+
+});
